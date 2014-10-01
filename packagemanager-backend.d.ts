74962771fa3816ab98fe339481fb9d08d98e55e9
vvakame <vvakame+dev@gmail.com>
2014-10-01 23:52:57 +0900

    comment out fs-git dependency from .d.ts

--- packagemanager-backend.d.ts
+++ packagemanager-backend.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="./node_modules/fs-git/fs-git.d.ts" />
+// /// <reference path="./node_modules/fs-git/fs-git.d.ts" />
 
 // /// <reference path="./typings/node/node.d.ts" />
 // /// <reference path="./typings/es6-promise/es6-promise.d.ts" />
